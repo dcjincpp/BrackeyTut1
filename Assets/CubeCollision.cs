39f2d75a98617f89d3461ef726fa6044cc3c807d
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2023-05-31 19:23:17 -0700

    Episode 5
    Collision episode complete

---
+++ Assets/CubeCollision.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CubeCollision : MonoBehaviour
+{
+
+    public CubeMovement movement;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
